c6761bcf207a7fd1a8846b274bf5c4787480be2d
Shaun Assam <thebeardedhermit@gmail.com>
2026-01-06 18:50:46 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,36 +4,18 @@ set -ouex pipefail
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
+# Remove unwanted packages
 dnf5 remove -y gnome-extensions-app
 
 # Adding Fedora Workstation and additional packages
-dnf5 install -y gnome-tweaks distrobox vim-enhanced samba fuse-sshfs dconf \
+dnf5 install -y gnome-tweaks distrobox podman vim-enhanced samba virtualization fuse-sshfs \
   gnome-shell-extension-dash-to-dock gnome-shell-extension-just-perfection gnome-shell-extension-appindicator \
   gnome-shell-extension-apps-menu gnome-shell-extension-places-menu \
   ffmpeg-free flac faad2 lame libmad
 
-#### Install Desktop Packages
-#/ctx/base/01-desktop.sh
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
+# Enable COPRs & install respective packages:
 dnf5 -y copr enable sassam/tuqueOS
 dnf5 -y install gnome-shell-extension-logo-menu
-
-#### Example for enabling a System Unit File
-
-#systemctl enable podman.socket
-systemctl enable smb nmb
 
 #### Apply Flatpaks
 /ctx/base/04-flatpaks
@@ -42,4 +24,4 @@ systemctl enable smb nmb
 /ctx/base/08-override-schemas.sh
 
 #### Clean up
-# /ctx/base/09-cleanup.sh
+/ctx/base/09-finalize-cleanup.sh

a37c83e353e4ab78e2ab2d66e3c16df4691e28e2
shaunassam <thebeardedhermit@gmail.com>
2026-01-31 01:20:48 -0500

    Updated system packages script

--- build_files/base/02-system-packages.sh
+++ build_files/base/02-system-packages.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 
-# Install required packages for tuqueOS image
+# Install required packages for tuqueOS image if using Fedora bootc
 dnf5 group install -y \
     gnome-desktop \
     workstation-product \
+    workstation-product-environment \
     fonts \
     workstation-ostree-support \
     system-tools \
@@ -18,7 +19,10 @@ dnf5 group install -y \
     container-management
 
 dnf5 install -y \
-    chromium \
+    firefox \
+    firefox-langpacks \
+    mozilla-openh264 \
+    thunderbird \
     gnome-tweaks \
     distrobox \
     vim-enhanced \
@@ -26,25 +30,43 @@ dnf5 install -y \
     ntfs-3g \
     ntfsprogs \
     fuse-sshfs \
+    flatpak \
+    gnome-calendar \
+    gnome-text-editor \
+    gnome-weather \
+    papers \
+    gnome-maps \
+    gnome-calculator \
+    snapshot \
+    gnome-characters \
+    gnome-clocks \
+    gnome-connections \
+    gnome-contacts \
+    loupe \
     gnome-shell-extension-dash-to-dock \
     gnome-shell-extension-just-perfection \
     gnome-shell-extension-appindicator \
     gnome-shell-extension-blur-my-shell \
-    gnome-shell-extension-submenu-power-toggle \
+    gnome-shell-extension-logo-menu \
     gnome-shell-extension-desktop-icons-ng \
-    plymouth \
-    plymouth-system-theme \
-    fwupd \
+    gnome-shell-extension-submenu-power-toggle \
+    vlc \
+    vlc-plugins-all \
+    rhythmbox \
     ffmpeg-free \
     flac \
     faad2 \
     lame \
     libmad \
-    vorbis-tools
+    vorbis-tools \
+    gnome-chess \
+    widevine-installer
 
 # Remove unwanted packages
-dnf5 remove -y firefox firefox-langpacks gnome-extensions-app
+dnf5 remove -y gnome-extensions-app
 
 # Enable COPRs & install respective packages:
 dnf5 -y copr enable sassam/tuqueOS
-dnf5 -y install gnome-shell-extension-logo-menu
+dnf5 -y install gnome-shell-extension-logo-menu \
+    gnome-shell-extension-submenu-power-toggle \
+    gnome-shell-extension-desktop-icons-ng
